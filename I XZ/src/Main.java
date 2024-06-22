7058c33193a5ff231122e0f99ccf3b3b40b78557
mihan <maleksyutin@bk.ru>
2024-06-22 23:03:51 +0300

    Ур 4

---
+++ I XZ/src/Main.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//                          План урока
+//
+//                1. Что такое переменная
+//                2. Для чего нужна переменная (Хранение данных, упрощение ввода)
+//                3. Хранение переменных в Java (Примитивные и ссылочные типы данных)
+//                4. Примитеивные типы данных
+//                5. Максимальные и минимальные значения
+//                6. Переполнение переменных
+//                7. Ввод данных в консоль
+//                8. Практика (Исправление BMI, калькулятор умножения
+
+
+// Упрощение ввода
+//        int a = 124; //int-тип данных;  а - название переменной; = - знак присваивания; 124 - значение переменной.
+//        System.out.println(a);
+//        System.out.println(a);
+//        System.out.println(a);
+//        System.out.println(a);
+//        System.out.println(a);
+//        System.out.println(a);
+//
+//
+//        int carrot = 23;
+//        int apple = 1234;
+//        System.out.println((apple + carrot) /2);
+
+        // Примитиыные типы данных (byte, short, char, int, long, float, double, boolean)
+
+        // Наиболее часто используеемые boolean, double, long, int.
+//        boolean = false или true (boolean-логический)
+//        double = 1.7976931348623157E308
+//        long = 9223372036854775807
+//        int  = 2147483647
+        //char ch = 'T';
+
+
+//        boolean bool = true;
+//        System.out.println(Double.MAX_VALUE);
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println();
+//
+//        double nam = 1867969.586556886877;
+//        System.out.println(nam);
+
+//        Scanner DIC = new Scanner(System.in);
+//        int shi = DIC.nextInt();
+//        int shi = 564;
+
+
+//        System.out.println("Здарова ёба.");
+//        System.out.println("Имя свое введи:");
+//        String uzernam = new Scanner(System.in).nextLine();
+//        System.out.println("Теперь\t" + uzernam);
+//        System.out.println("Введи свой вес(кг):"); //Вес - weight
+//        double weight = new Scanner(System.in).nextInt();
+//        System.out.println("Введи свой рост");//Рост - height
+//        double height = new Scanner(System.in).nextDouble();
+//        System.out.println("BMI = " + weight / (height * height));
+
+//        System.out.println("Введите первое число:");
+//        double a = new Scanner(System.in).nextDouble();
+//        System.out.println("Введите второе число:");
+//        double b = new Scanner(System.in).nextDouble();
+//        System.out.println(a + b);
+//        System.out.println(a / b);
+//        System.out.println(a * b);
+//        System.out.println(a - b);
+
+
+        //ДЗ
+
+        int q = 4;
+        int w =5;
+        int e = 47;
+        int r = 56;
+        int t = 489;
+        System.out.println( q + " ; " + w + " ; " + e + " ; " + r );
+        System.out.println("Введи свое имя :");
+        Scanner pip = new Scanner(System.in);
+        String username = pip.nextLine();
+        char ch = 'T';
+        char c = '@';
+        System.out.println(c+"/"+ch);
+        int f = 10;
+        System.out.println(f);
+        double g = 54.45455;
+        System.out.println(g);
+
+        System.out.println("Введите первое число:");
+        double nam1 = new Scanner(System.in).nextDouble();
+        System.out.println("Введите второе число:");
+        double nam2 = new Scanner(System.in).nextDouble();
+        System.out.println(nam1 * nam2);
+        System.out.println(nam1 / nam2);
+        System.out.println(nam1 + nam2);
+        System.out.println(nam1 - nam2);
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
