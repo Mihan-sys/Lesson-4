b3a652858f236e05421676aa4c9d00d482b4d25e
Mihan-sys <maleksyutin@bk.ru>
2024-06-23 13:11:39 +0300

    Add files via upload

--- Mein.java
+++ Mein.java
@@ -1,37 +1,30 @@
-import java.util.Scanner;
-
-
 public class Mein {
     public static void main(String[] args) {
-       /*
-        System.out.println(20+20);
-        System.out.println(10*5);
-        System.out.println(10/3);
-        System.out.println(10.0/3);
-        System.out.println(3+5*2);
-        System.out.println((3+5)*2);
+        // The first error is in this block
+        System.out.println("***** Welcome to simple program *****");
+        System.out.println("*\tThis program contains 3 errors\t*");
 
-        System.out.println("\n10"+ 50);
-        System.out.println(10+ 50);
-        */
+        System.out.println("Please enter a natural number between 1 and 5:");
+        // The second error is in this block
+        Scanner scanner = new scanner(System.in);{
 
-//        System.out.println("\t\t\t Добро пожаловать в конвертор ГБ");
-//        System.out.println("В " + 7 + " gb: " + (7 * 1024) + " mb");
+        int naturalNumber = scanner.nextInt();
 
-        // масса (кг)/ рост2(м2)
-//        System.out.println("\t\t\t***tДобропо пожаловать в калькулятор BMI***");
-//        System.out.println("BMI " + 75.0 / (180*180));
+        if(naturaNumber < 1 || naturalNumber > 5) {
+            System.out.println("Your number is not in the specified range!");
+        } else if(naturalNumber == 1) {
+            System.out.println("Hi, how are you doing?");
+        } else if(naturalNumber == 2) {
+            // The third error is in this block
+            System.out.println("Have a good day!");
+        } else if(naturalNumber == 3) {
+            System.out.println("You're very good at fixing bugs.");
+         else if(naturalNumber == 4) {
+                System.out.println("You will become a good programmer!");
+            } else {
+                System.out.println("See you soon!");
+            }
 
-        System.out.println("Введите температуру воздуха по цельсия");
-        Scanner data = new Scanner(System.in);
-        float C = data.nextFloat();
-        //System.out.println(" F " + C + 1.8 + 32);
-        System.out.println(" USD " + C/94);
-        System.out.println(" EUR " + C/105);
-
-
-
-
+        }
     }
 
-}
